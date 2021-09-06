8cffa7273ea5457412ac5d0da3e4c18dfcd5d454
GabrielBueno200 <gabrielbueno200@gmail.com>
2021-09-06 19:31:03 -0300

    fixed typing

--- frontend/src/app/hooks/useLiveQueryable.tsx
+++ frontend/src/app/hooks/useLiveQueryable.tsx
@@ -6,30 +6,30 @@ const useLiveQueryable = (
     reRenderTime = 1000
 ) => {
 
-        const [ data, setData ] = useState({});
+    const [ data, setData ] = useState({});
 
-        useEffect(() => { setInterval(loadData, reRenderTime) }, []);
+    useEffect(() => { setInterval(loadData, reRenderTime) }, []);
 
-        const loadData = async () => {
+    const loadData = async () => {
 
-            try {
-                const res = await session.run(query);
-                
-                res.records.forEach(r => {
-                    console.log(r);
-                });
-                
-                setData(res);
+        try {
+            const res = await session.run(query);
+            
+            res.records.forEach(r => {
+                console.log(r);
+            });
+            
+            setData(res);
 
-                //session.close();
-            }
-            catch(ex){
-                console.log(ex);
-            }
-        
-        };
+            //session.close();
+        }
+        catch(ex){
+            console.log(ex);
+        }
+    
+    };
 
-        return data;
+    return data;
 };
 
 export default useLiveQueryable;
