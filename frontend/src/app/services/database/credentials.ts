41005f26831da61831707d3b91b57d28cc070147
Antonio Muniz <antonio.gustavo_@live.com>
2021-09-09 20:30:58 -0300

    Added env variables

--- frontend/src/app/services/database/credentials.ts
+++
@@ -1,7 +0,0 @@
-export enum Credentials{
-    scheme =  'neo4j+s',
-    host = 'a0aa1dbc.databases.neo4j.io',
-    port = 7687,
-    user = 'neo4j',
-    password = '2hYk9gOHAPUVZwVZNsnnghrMt_XtTAXYeVbpy8lJh_c',
-};
---
+++ frontend/src/app/utils/requests.tsx
@@ -0,0 +1,4 @@
+export const NEO4J_HOST = process.env.NEO4J_HOST ?? 'http://localhost:8080';
+export const NEO4J_PORT = process.env.NEO4J_PORT ?? 'http://localhost:8080';
+export const NEO4J_USER = process.env.NEO4J_USER ?? 'http://localhost:8080';
+export const NEO4J_PASSWORD = process.env.NEO4J_PASSWORD ?? 'http://localhost:8080';
