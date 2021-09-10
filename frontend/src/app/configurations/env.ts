9563215e59cc3f23be6efa80e9ba26b2ee218549
GabrielBueno200 <gabrielbueno200@gmail.com>
2021-09-09 23:44:44 -0300

    updated env vars names

--- frontend/src/app/configurations/env.ts
+++ frontend/src/app/configurations/env.ts
@@ -1,9 +1,9 @@
 const env = {
-    NEO4J_SCHEME: process.env.NEO4J_SCHEME,
-    NEO4J_HOST: process.env.NEO4J_HOST,
-    NEO4J_PORT: process.env.NEO4J_PORT,
-    NEO4J_USER: process.env.NEO4J_USER,
-    NEO4J_PASSWORD: process.env.NEO4J_PASSWORD,
+    NEO4J_SCHEME: process.env.REACT_APP_NEO4J_SCHEME,
+    NEO4J_HOST: process.env.REACT_APP_NEO4J_HOST,
+    NEO4J_PORT: process.env.REACT_APP_NEO4J_PORT,
+    NEO4J_USER: process.env.REACT_APP_NEO4J_USER,
+    NEO4J_PASSWORD: process.env.REACT_APP_NEO4J_PASSWORD,
 };
 
 export default env;
